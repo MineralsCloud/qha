d82c88440887e852e3a6e92a01a4c2878b24aac8
tianqin91 <qinxx197@umn.edu>
2018-05-15 14:40:25 -0400

    Update same_phonon_dos, now use the same input as different_phonon_dos, but with one keyword: "same_phonon_dos: True"

--- qha/calculator.py
+++ qha/calculator.py
@@ -327,35 +327,35 @@ class Calculator:
         return pressure_specific_heat_capacity(self.cv_tp_jmolk, self.alpha_tp, self.gamma_tp, self.temperature_array)
 
 
-class SamePhDOSCalculator(Calculator):
-    def __init__(self, user_settings):
-        super().__init__(user_settings)
-
-        self._volume_energy = None
-        self._degeneracies = None
-
-    @property
-    def volume_energy(self):
-        return self._volume_energy
-
-    @property
-    def degeneracies(self):
-        return self._degeneracies
-
-    def read_energy_degeneracy(self):
-        volume_energies = pd.read_csv(self.settings['volume_energies'], sep='\s+', index_col='volume')
-        self._degeneracies = tuple(self.settings['input'].values())
-
-        self._volume_energy = volume_energies
-
-    @LazyProperty
-    def vib_ry(self):
-        v = np.empty(self.temperature_array.shape)
-
-        for i, t in enumerate(self.temperature_array):
-            v[i] = same_phonon_dos.FreeEnergy(t, self.degeneracies, self.q_weights, self.volume_energy.as_matrix(),
-                                              self.frequencies).total
-        return v
+# class SamePhDOSCalculator(Calculator):
+#     def __init__(self, user_settings):
+#         super().__init__(user_settings)
+#
+#         self._volume_energy = None
+#         self._degeneracies = None
+#
+#     @property
+#     def volume_energy(self):
+#         return self._volume_energy
+#
+#     @property
+#     def degeneracies(self):
+#         return self._degeneracies
+#
+#     def read_energy_degeneracy(self):
+#         volume_energies = pd.read_csv(self.settings['volume_energies'], sep='\s+', index_col='volume')
+#         self._degeneracies = tuple(self.settings['input'].values())
+#
+#         self._volume_energy = volume_energies
+#
+#     @LazyProperty
+#     def vib_ry(self):
+#         v = np.empty(self.temperature_array.shape)
+#
+#         for i, t in enumerate(self.temperature_array):
+#             v[i] = same_phonon_dos.FreeEnergy(t, self.degeneracies, self.q_weights, self.volume_energy.as_matrix(),
+#                                               self.frequencies).total
+#         return v
 
 
 class DifferentPhDOSCalculator(Calculator):
@@ -425,4 +425,17 @@ class DifferentPhDOSCalculator(Calculator):
         for i, t in enumerate(self.temperature_array):
             mat[i] = different_phonon_dos.PartitionFunction(t, *(arg for arg in args)).derive_free_energy
 
+        return mat
+
+
+class SamePhDOSCalculator(DifferentPhDOSCalculator):
+    def __init__(self, user_settings):
+        super().__init__(user_settings)
+
+    @LazyProperty
+    def vib_ry(self):
+        args = self.degeneracies, self.q_weights[0], self.static_energies.T, self.frequencies[0]
+        mat = np.empty((self.temperature_array.size, self._volumes.shape[1]))
+        for i, t in enumerate(self.temperature_array):
+            mat[i] = same_phonon_dos.FreeEnergy(t, *(arg for arg in args)).total
         return mat
