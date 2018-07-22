MineralsCloud/qha

commit 46f3766aab01789128ce96a29a3b6b8a15617093

Fix cli cannot run bug

qha/cli/__init__.py
@@ -18,6 +18,11 @@ def main():
     qha_runner = QHAPlotter()
     parser.add_program('plot', qha_runner)
 
+    qha_plotter = QHAPlotter()
+    parser.add_program('plot', qha_plotter)
+
+    parser.parse_args()
+
 
 if __name__ == '__main__':
     main()

commit 0f0497bc80d66074b51541aa96d6ff1bb656512d

fix: Fix a duplication bug
This bug was introduced in commit "46f3766aab01789128ce96a29a3b6b8a15617093".

qha/cli/__init__.py
@@ -15,9 +15,6 @@ def main():
     qha_runner = QHARunner()
     parser.add_handler('run', qha_runner)
 
-    qha_runner = QHAPlotter()
-    parser.add_handler('plot', qha_runner)
-
     qha_plotter = QHAPlotter()
     parser.add_handler('plot', qha_plotter)
 