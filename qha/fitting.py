6558fc52f1eda90d09218b6d0f06b2d81d70491c
tianqin <tianqin91@gmail.com>
2018-08-07 14:03:50 -0500

    docs: Slightly update docs of fitting

--- qha/fitting.py
+++ qha/fitting.py
@@ -3,7 +3,7 @@
 .. module fitting
    :platform: Unix, Windows, Mac, Linux
    :synopsis: This module is one of the most important part of this package since it implements a robust
-    Birch--Murnaghan EoS fitting for ``grid_interpolation`` modules' use.
+    finite strain EoS fitting for ``grid_interpolation`` modules' use.
 .. moduleauthor:: Tian Qin <qinxx197@umn.edu>
 """
 
@@ -18,20 +18,20 @@ __all__ = ['polynomial_least_square_fitting', 'birch_murnaghan_finite_strain_fit
 
 def polynomial_least_square_fitting(x, y, new_x, order: Optional[int] = 3):
     """
-    Free energy is expanded to the *order*th order in strain.
+    Free energies are expanded to the *order*th order in strains.
 
     Check the `Wolfram Mathworld <http://mathworld.wolfram.com/LeastSquaresFittingPolynomial.html>`_
     to find the method of calculating the coefficients.
 
-    :param x: The Eulerian strain of calculated volumes (sparse).
-    :param y: Free energy of these calculated volumes (sparse).
-    :param new_x: Eulerian strain at a greater dense vector.
-    :param order: The order chose to fit Birch--Murnaghan EoS, default is ``3``, i.e., third-order Birch--Murnaghan
+    :param x: The Eulerian strains of calculated volumes (sparse).
+    :param y: Free energies of these calculated volumes (sparse).
+    :param new_x: Eulerian strains at a greater dense vector.
+    :param order: The order chose to fit finite strain EoS, default is ``3``, i.e., third-order Birch--Murnaghan
         EoS fitting.
     :return: Free energy at a denser strain vector (denser volumes vector).
     """
-    order += 1  # The order needed is 1 more than ``numpy.vander`` default value.
-    X = np.vander(x, order, increasing=True)  # This will make a Vandermonde matrix that will be used in BM fitting.
+    order += 1  # The definition of order in ``numpy.vander`` is different from the order in finite strain by one.
+    X = np.vander(x, order, increasing=True)  # This will make a Vandermonde matrix that will be used in EoS fitting.
     X_T = X.T
     a = inv(X_T @ X) @ X_T @ y
     new_y = np.vander(new_x, order, increasing=True) @ a
@@ -40,15 +40,15 @@ def polynomial_least_square_fitting(x, y, new_x, order: Optional[int] = 3):
 
 def birch_murnaghan_finite_strain_fitting(eulerian_strain, free_energy, strain, order: Optional[int] = 3):
     """
-    Calculate the ``F(T, V)`` for given strain.
-    Free Energy is expanded to the ``order``:math:`^\mathrm{th}` order in strain.
+    Calculate the ``F(T, V)`` for given strains.
+    Free Energies are expanded to the ``order``:math:`^\mathrm{th}` order in strains.
 
-    :param eulerian_strain: Eulerian strain of calculated volumes (sparse).
-    :param free_energy: Free energy of these calculated volumes (sparse).
-    :param strain: Eulerian strain at a greater dense vector.
-    :param order: The order chose to fit Birch--Murnaghan EoS, default is ``3``, i.e., third-order Birch--Murnaghan
+    :param eulerian_strain: Eulerian strains of calculated volumes (sparse).
+    :param free_energy: Free energies of these calculated volumes (sparse).
+    :param strain: Eulerian strains at a greater dense vector.
+    :param order: The order chose to fit finite strain EoS, default value is ``3``, i.e., third-order Birch--Murnaghan
         EoS fitting.
-    :return: Free energy at a dense :math:`(T, V)` grid.
+    :return: Free energies at a dense :math:`(T, V)` grid.
     """
     temperature_amount, _ = free_energy.shape
     dense_volume_amount = len(strain)
