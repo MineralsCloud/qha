758f86e8c0d4726eef6b269a12bec798f186a9e4
Qi Zhang <singularitti@outlook.com>
2019-06-16 01:19:50 -0400

    Fix a bug: compare results before running

--- buildscripts/travis.sh
+++ buildscripts/travis.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -ev
 
-echo "Do Python tests"
-pytest qha/tests/test_different_phonon_dos.py
-pytest qha/tests/test_read_input.py
-pytest qha/tests/test_single_configuration.py
-pytest qha/tests/test_unit_conversion.py
-pytest qha/tests/test_input_maker.py
-pytest qha/tests/test_overall_run.py
-
 qha -V
 
 cd examples/
@@ -19,6 +11,15 @@ cd ice\ VII
 qha run ./settings.yaml
 cd ..
 pwd  # I am at root/qha/examples
+cd ..
+
+echo "Do Python tests"
+pytest qha/tests/test_different_phonon_dos.py
+pytest qha/tests/test_read_input.py
+pytest qha/tests/test_single_configuration.py
+pytest qha/tests/test_unit_conversion.py
+pytest qha/tests/test_input_maker.py
+pytest qha/tests/test_overall_run.py
 
 echo "Build docs"
 cd ..
