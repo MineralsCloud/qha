40826a7593038f456d5fa52e9b77a4e5efb23bc2
Qi Zhang <singularitti@outlook.com>
2019-06-15 21:55:23 -0400

    Move `script` config to buildscripts/travis.sh

---
+++ buildscripts/travis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ev
+
+echo "Do Python tests"
+pytest qha/tests/test_different_phonon_dos.py
+pytest qha/tests/test_read_input.py
+pytest qha/tests/test_single_configuration.py
+pytest qha/tests/test_unit_conversion.py
+
+echo "Build docs"
+cd docs/
+make clean && make html
