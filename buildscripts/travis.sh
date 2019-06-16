c4c517534ef16bd28b6f7f6f81313d89a6b9cd56
Qi Zhang <singularitti@outlook.com>
2019-06-15 23:43:35 -0400

    Add a test for input_maker.py

--- buildscripts/travis.sh
+++ buildscripts/travis.sh
@@ -6,6 +6,7 @@ pytest qha/tests/test_different_phonon_dos.py
 pytest qha/tests/test_read_input.py
 pytest qha/tests/test_single_configuration.py
 pytest qha/tests/test_unit_conversion.py
+pytest qha/tests/test_input_maker.py
 
 echo "Build docs"
 cd docs/
---
+++ qha/tests/test_input_maker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Created at Jun 15, 2019, by Qi Zhang
+
+
+import filecmp
+import os
+import pathlib
+import unittest
+
+from qha.basic_io.input_maker import FromQEOutput
+
+
+class TestMakeInput(unittest.TestCase):
+    def setUp(self) -> None:
+        self.dir = pathlib.Path(__file__).parent.parent.parent / 'examples/silicon/make_input/'
+        os.chdir(self.dir)
+
+    def test_make_input(self):
+        converter = FromQEOutput(self.dir / 'filelist.yaml', self.dir / 'static', self.dir / 'q_points')
+        converter.read_file_list()
+        converter.read_static()
+        converter.read_q_points()
+        converter.read_frequency_files()
+        converter.write_to_file(str(self.dir / 'input'))
+        self.assertTrue(filecmp.cmp(self.dir / 'input', self.dir / '../input'))
+
+
+if __name__ == '__main__':
+    unittest.main()
