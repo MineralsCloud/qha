5d465d9713f1f56a09310ea2595d14087a9b4429
Qi Zhang <singularitti@outlook.com>
2019-06-16 00:58:07 -0400

    Move things in `before_script` to buildscripts/qha_old.sh

---
+++ buildscripts/qha_old.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+virtualenv qha_old
+cd qha_old/bin/
+source activate
+cd ../  # I am at root/qha/qha_old
+pip3 install qha==1.0.17
+cp -R ../examples/* ./  # cp root/qha/examples to root/qha/qha_old/
+cd silicon
+qha run ./settings.yaml
+cd ..
+cd ice\ VII
+qha run ./settings.yaml  # I am at root/qha/qha_old/ice\ VII/
+cd ..  # I am at root/qha/qha_old
+deactivate
+cd ../  # I am at root/qha/
