401605128ec86a72b664eb92d03a8bb694c446de
singularitti <singularitti@outlook.com>
2021-01-21 15:51:45 -0500

    Bump version to 1.0.20

--- qha/__init__.py
+++ qha/__init__.py
@@ -10,4 +10,4 @@ __copyright__ = 'Copyright (c) 2018, Renata group'
 __credits__ = {'Renata M. M. Wentzcovitch': 'rmw2150@columbia.edu'}
 __date__ = 'Feb 17, 2018'
 __maintainer__ = 'Tian Qin, Qi Zhang'
-__version__ = '1.0.19'
+__version__ = '1.0.20'
